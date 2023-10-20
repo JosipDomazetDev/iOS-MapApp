82be5aa483e6d1b6eab22436765240a5291cd9bd
user <user@iMac-von-user-4.local>
2023-10-20 08:39:29 +0200

    first commit

---
+++ AssignmentMapJosipDomazetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentMapJosipDomazetApp.swift
+//  AssignmentMapJosipDomazet
+//
+//  Created by user on 20.10.23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentMapJosipDomazetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
